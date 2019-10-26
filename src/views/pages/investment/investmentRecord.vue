0a63bda1627e5c0405a0617444a05e715752e88d
Your Name <xubing>
2019-10-26 15:33:54 +0800

    收款方式等

--- src/views/pages/investment/investmentRecord.vue
+++ src/views/pages/investment/investmentRecord.vue
@@ -9,11 +9,11 @@
     <div class="income_box">
       <div class="income_left">
         <div>{{$t('investment.总投资金额')}}:</div>
-        <div>12439054</div>
+        <div>{{amount}}</div>
       </div>
       <div class="income_right">
         <div>{{$t('investment.总收益金额')}}:</div>
-        <div>12439054</div>
+        <div>{{grantedYield}}</div>
       </div>
     </div>
 
@@ -23,159 +23,222 @@
     </div>
 
     <translate position="top">
-      <div class="item" v-for="item in 4" :key="item">
+      <div class="item" v-for="item in list" :key="it">
         <div class="title">投资</div>
         <div class="num_box">
           <div>
-            <div class="num">2</div>
+            <div class="num">{{item.amount}}</div>
             <div class="txt">{{$t('investment.投资金额')}}</div>
           </div>
           <div>
-            <div class="num">3432232</div>
+            <div class="num">{{item.grantedYield}}</div>
             <div class="txt">{{$t('investment.累计收益')}}</div>
           </div>
           <div>
-            <div class="num">3432232</div>
+            <div class="num">{{item.days}}</div>
             <div class="txt">{{$t('investment.累计天数')}}</div>
           </div>
         </div>
         <div class="time_box">
           <div>
-            <div class="num">2019-8-19 17:29:29</div>
+            <div class="num">{{item.createdAt}}</div>
             <div class="txt">{{$t('investment.投资时间')}}</div>
           </div>
           <div>
-            <div class="num">2019-8-19 17:29:29</div>
+            <div class="num">{{item.expiredAt}}</div>
             <div class="txt">{{$t('investment.结束时间')}}</div>
           </div>
         </div>
       </div>
     </translate>
+    <!-- 加载完毕提示 -->
+    <div v-if="isEndShow == 1">
+      <p class="jzwb">已全部加载完毕</p>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      grantedYield:0,
+      amount:0,
+      list:[],
+      is_show: false,
+      lastId:0,
+      page:1,
+      isEnd:0,
+      isEndShow:0,
+    };
+  },
+  mounted() {
+    this.getData();
+    window.addEventListener('scroll', this.scrollEvent,false);
+  },
+  methods: {
+      scrollEvent(){
+        if (document.documentElement.scrollTop + document.documentElement.clientHeight >= document.body.scrollHeight) {
+          if(this.isEnd == 0){
+            console.log('加载下一页',this.isEnd,this.page)
+            this.page =  parseInt(this.page)+1
+            this.getData()
+          }
+        }
+      },
+      getData(){
+        var that = this
+        this.$post({
+            module: "Investment",
+            interface: 4000,
+            data:{
+              page:this.page,
+              lastId:this.lastId
+            },
+            success: res => {
+                if (res.data.code == 0) {
+                    // this.data = res.data.data
+                    if(res.data.data.list.length == 0){
+                      that.isEnd = 1;
+                      that.isEndShow = 1;
+                      return ;
+                    }
+                    that.list = that.list.concat(res.data.data.list)
+                    that.lastId = res.data.data.lastId
+                    that.amount = res.data.data.amount
+                    that.grantedYield = res.data.data.grantedYield
+                }
+                else{
+                    this.$toast({
+                        duration: 1000,
+                        message: res.data.message
+                    });
+                }
+            }
+        });
+      }
   }
 };
 </script>
 <style lang="scss" scoped>
-.nav {
-  height: 46px;
-  background-color: #0c0c0c;
-  display: flex;
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
-  padding-right: 35px;
-
-  img {
-    width: 24px;
-    height: 24px;
-    margin-left: 11px;
+  .jzwb{
+    text-align:center;
+    color:#ccc;
   }
-  span {
-    color: #ffffff;
-    font-size: 18px;
-    flex: 1;
-    text-align: center;
-  }
-}
-.income_box {
-  display: flex;
-  width: 92%;
-  margin: 10px auto;
-  color: #ffffff;
-
-  .income_left {
-    height: 100px;
-    background: linear-gradient(
-      137deg,
-      rgba(246, 180, 109, 1) 0%,
-      rgba(236, 128, 24, 1) 100%
-    );
-    opacity: 1;
-    border-radius: 6px 0 0 6px;
-    padding: 15px 14px 32px;
-    transform: translateX(2px);
-    flex: 1;
-
-    div:first-child {
-      font-size: 13px;
-    }
-
-    div:last-child {
-      font-size: 20px;
-      line-height: 40px;
-    }
-  }
-  .income_right {
-    @extend .income_left;
-    border-radius: 6px;
-    transform: translateX(-2px);
-  }
-}
-.money_type {
-  padding: 13px 16px;
-  color: #ffffff;
-  font-size: 16px;
-  display: flex;
-  align-items: center;
-
-  i {
-    width: 6px;
-    height: 16px;
-    display: inline-block;
-    background: linear-gradient(
-      180deg,
-      rgba(255, 147, 12, 1) 0%,
-      rgba(255, 80, 0, 1) 100%
-    );
-    opacity: 1;
-    border-radius: 3px;
-    margin-right: 8px;
-  }
-}
-
-.item {
-  background-color: #281a00;
-  border-radius: 6px;
-  width: calc(100% - 30px);
-  margin: 0 auto;
-  margin-bottom: 15px;
-
-  .title {
-    font-size: 18px;
-    color: #ffffff;
-    padding: 12px 15px 0;
-  }
-
-  .num_box {
+  .nav {
+    height: 46px;
+    background-color: #0c0c0c;
+    display: flex;
+    align-items: center;
     display: flex;
     justify-content: space-between;
-    color: #fff7de;
-    padding: 5px 15px;
+    padding-right: 35px;
 
-    & > div {
-      .num {
-        font-size: 14px;
-        text-align: center;
+    img {
+      width: 24px;
+      height: 24px;
+      margin-left: 11px;
+    }
+    span {
+      color: #ffffff;
+      font-size: 18px;
+      flex: 1;
+      text-align: center;
+    }
+  }
+  .income_box {
+    display: flex;
+    width: 92%;
+    margin: 10px auto;
+    color: #ffffff;
+
+    .income_left {
+      height: 100px;
+      background: linear-gradient(
+        137deg,
+        rgba(246, 180, 109, 1) 0%,
+        rgba(236, 128, 24, 1) 100%
+      );
+      opacity: 1;
+      border-radius: 6px 0 0 6px;
+      padding: 15px 14px 32px;
+      transform: translateX(2px);
+      flex: 1;
+
+      div:first-child {
+        font-size: 13px;
       }
-      .txt {
-        font-size: 12px;
+
+      div:last-child {
+        font-size: 20px;
+        line-height: 40px;
       }
+    }
+    .income_right {
+      @extend .income_left;
+      border-radius: 6px;
+      transform: translateX(-2px);
+    }
+  }
+  .money_type {
+    padding: 13px 16px;
+    color: #ffffff;
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+
+    i {
+      width: 6px;
+      height: 16px;
+      display: inline-block;
+      background: linear-gradient(
+        180deg,
+        rgba(255, 147, 12, 1) 0%,
+        rgba(255, 80, 0, 1) 100%
+      );
+      opacity: 1;
+      border-radius: 3px;
+      margin-right: 8px;
     }
   }
 
-  .time_box {
-    @extend .num_box;
-    & > div {
-      .num {
-        text-align: start;
+  .item {
+    background-color: #281a00;
+    border-radius: 6px;
+    width: calc(100% - 30px);
+    margin: 0 auto;
+    margin-bottom: 15px;
+
+    .title {
+      font-size: 18px;
+      color: #ffffff;
+      padding: 12px 15px 0;
+    }
+
+    .num_box {
+      display: flex;
+      justify-content: space-between;
+      color: #fff7de;
+      padding: 5px 15px;
+
+      & > div {
+        .num {
+          font-size: 14px;
+          text-align: center;
+        }
+        .txt {
+          font-size: 12px;
+        }
       }
     }
-    border-top: 1px solid rgba(67, 46, 0, 1);
+
+    .time_box {
+      @extend .num_box;
+      & > div {
+        .num {
+          text-align: start;
+        }
+      }
+      border-top: 1px solid rgba(67, 46, 0, 1);
+    }
   }
-}
 </style>
--- src/views/pages/my/add_payment.vue
+++ src/views/pages/my/add_payment.vue
@@ -6,77 +6,95 @@
       <span>{{$t('my.添加收款方式')}}</span>
     </div>
     <div class="box">
-      <div class="nav_box"
-           v-if="is_type==1">
-        <span></span>
-        <p>{{$t('my.银行')}}({{$t('my.新增收款方式将会进行重新审核!')}})</p>
+      <!-- 银行卡 -->
+      <div v-if="is_type==3">
+        <div class="nav_box">
+          <span></span>
+          <p>{{$t('my.银行')}}</p>
+        </div>
+        <div class="inp_box">
+          <input type="text"
+                 :placeholder="$t('my.请输入银行')"
+                 v-model="bankCode">
+        </div>
+        <div class="nav_box">
+          <span></span>
+          <p>{{$t('my.账户名称')}}</p>
+        </div>
+        <div class="inp_box">
+          <input type="text"
+                 :placeholder="$t('my.输入账户名称')"
+                 v-model="owner">
+        </div>
+        <div class="nav_box">
+          <span></span>
+          <p>{{$t('my.账号')}}</p>
+        </div>
+        <div class="inp_box">
+          <input type="text"
+                 :placeholder="$t('my.输入账号')"
+                 v-model="cardNo">
+        </div>
+        <div class="nav_box">
+          <span></span>
+          <p>{{$t('assets.开户行')}}</p>
+        </div>
+        <div class="inp_box">
+          <input type="text"
+                 :placeholder="$t('my.输入开户行')"
+                 v-model="bankSubbranch">
+        </div>
       </div>
-      <div class="inp_box"
-           v-if="is_type==1">
-        <input type="text"
-               :placeholder="$t('my.请输入银行')"
-               v-model="blank">
-      </div>
-      <div class="nav_box"
-           v-if="is_type==1">
-        <span></span>
-        <p>{{$t('my.账户名称')}}</p>
-      </div>
-      <div class="inp_box"
-           v-if="is_type==1">
-        <input type="text"
-               :placeholder="$t('my.输入账户名称')"
-               v-model="name">
-      </div>
-      <div class="nav_box"
-           v-if="is_type==1">
-        <span></span>
-        <p>{{$t('my.账号')}}</p>
-      </div>
-      <div class="inp_box"
-           v-if="is_type==1">
-        <input type="text"
-               :placeholder="$t('my.输入账号')"
-               v-model="code">
-      </div>
-      <div class="nav_box"
-           v-if="is_type==2">
-        <span></span>
-        <p>{{$t('my.微信/支付宝名称')}}</p>
-      </div>
-      <div class="inp_box"
-           v-if="is_type==2">
-        <input type="text"
-               :placeholder="$t('my.输入账户名称')"
-               v-model="code">
-      </div>
-      <div class="nav_box"
-           v-if="is_type==2">
-        <span></span>
-        <p>{{$t('my.微信/支付宝账号')}}</p>
-      </div>
-      <div class="inp_box"
-           v-if="is_type==2">
-        <input type="text"
-               :placeholder="$t('my.请输入对应的账号')"
-               v-model="code">
-      </div>
-      <div class="nav_box"
-           v-if="is_type==2">
-        <span></span>
-        <p>{{$t('my.上传收款码')}}</p>
-      </div>
-      <div class="img_nav_box"
-           v-if="is_type==2">
-        <label for="inp">
-          <img src="@/assets/img/tianjia.png"
-               alt="">
-          <input type="file"
-                 id="inp">
-        </label>
+      <!-- 支付宝微信 -->
+      <div v-if="is_type==2 || is_type==1">
+        <div class="nav_box">
+          <span></span>
+          <p>{{$t('my.微信/支付宝名称')}}</p>
+        </div>
+        <div class="inp_box">
+          <input type="text"
+                 :placeholder="$t('my.输入账户名称')"
+                 v-model="owner">
+        </div>
+        <div class="nav_box">
+          <span></span>
+          <p>{{$t('my.微信/支付宝账号')}}</p>
+        </div>
+        <div class="inp_box">
+          <input type="text"
+                 :placeholder="$t('my.请输入对应的账号')"
+                 v-model="cardNo">
+        </div>
+        <div class="nav_box">
+          <span></span>
+          <p>{{$t('my.上传收款码')}}</p>
+        </div>
+        <!-- <div class="img_nav_box">
+          <label for="inp">
+            <img src="@/assets/img/tianjia.png"
+                 alt="">
+            <input type="file"
+                   id="inp">
+          </label>
+        </div> -->
+        <div class="upload">
+          <div v-if="logo != ''">
+              <img :src="logo" alt class="pic" />
+              <img src="@/assets/img/del.png" alt class="del" @click="delimg()" />
+          </div>
+          <label for="file" v-if="logo == ''">
+              <input
+                  type="file"
+                  style="display:none;"
+                  @change="uploads($event)"
+                  ref="front"
+                  id="file"
+              />
+          </label>
+        </div>
       </div>
       <div class="btn_outer">
-        <button class="btn">{{$t('my.确定')}}</button>
+        <button class="btn" @click="sub_bank()">{{$t('my.确定')}}</button>
       </div>
     </div>
   </div>
@@ -85,130 +103,226 @@
 export default {
   data () {
     return {
-      blank: '',
-      name: '',
-      code: '',
+      owner: '',
+      cardNo: '',
+      bankCode: '',
+      bankSubbranch: '',
+      logo: '',
       is_type: this.$route.query.type,//1 银行卡 2 微信支付宝
     }
   },
   methods: {
+    //上传图片
+    uploads() {
+        let file = this.$refs.front.files[0]; //获取文件详细信息
+        if (file == null || file == undefined) {
+            return;
+        }
+        let param = new FormData();
+        param.append("avatar", file);
+        this.uploadImg(param);
+    },
+    uploadImg(param) {
+        this.$axios
+            .post(this.httpPaths, param, {
+                headers: {
+                    Authorization:
+                        "Bearer " + localStorage.getItem("Bearer")
+                }
+            })
+            .then(res => {
+                if (res.data.code == 0) {
+                    this.logo = res.data.data.avatar;
+                } else {
+                    this.$toast({
+                        message: res.data.message,
+                        duration: 1000
+                    });
+                }
+            });
+    },
+    // 删除 图片
+    delimg(index) {
+        this.logo = '';
+    },
     changeFiles () { },
+    //提交收款方式
+    sub_bank(){
+      this.$post({
+        module: "Finance",
+        interface: 1001,
+        data: {
+            owner: this.owner,
+            cardNo: this.cardNo,
+            bankCode: this.bankCode,
+            bankSubbranch: this.bankSubbranch,
+            logo: this.logo,
+            cardType: this.is_type,
+        },
+        success: res => {
+            
+            if (res.data.code == 0) {
+                this.$toast({
+                    duration: 1000,
+                    message: this.$t('my.添加账号成功')
+                });
+                this.$router.replace("/payment");
+            }
+            else{
+                this.$toast({
+                    duration: 1000,
+                    message: res.data.message
+                });
+            }
+        }
+      });
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
-.nav {
-  height: 46px;
-  background-color: #0c0c0c;
-  display: flex;
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
-  padding-right: 35px;
-
-  img {
-    width: 24px;
-    height: 24px;
-    margin-left: 11px;
-  }
-  span {
-    color: #ffffff;
-    font-size: 18px;
-    flex: 1;
-    text-align: center;
-  }
-}
-.box {
-  width: 100%;
-  padding: 0 15px;
-  box-sizing: border-box;
-  .nav_box {
-    width: 100%;
+  .nav {
+    height: 46px;
+    background-color: #0c0c0c;
     display: flex;
-    justify-content: flex-start;
     align-items: center;
-    padding: 20px 0;
-    span {
-      display: inline-block;
-      width: 6px;
-      height: 15px;
-      background: linear-gradient(
-        180deg,
-        rgba(255, 147, 12, 1) 0%,
-        rgba(255, 80, 0, 1) 100%
-      );
-      border-radius: 5px;
-    }
-    p {
-      padding-left: 15px;
-      flex: 1;
-      color: #ffffff;
-      font-size: 16px;
-    }
-  }
-  .inp_box {
-    width: 100%;
-    padding: 10px 15px;
     display: flex;
     justify-content: space-between;
-    align-items: center;
-    background: rgba(40, 26, 0, 1);
-    opacity: 1;
-    border-radius: 6px;
-    input {
-      flex: 1;
-      padding: 0 10px 0 0;
-      border: none;
-      background: none;
-      color: #ffffff;
-      font-size: 16px;
-    }
-    input::-webkit-input-placeholder {
-      color: #ffffff;
-    }
+    padding-right: 35px;
+
     img {
       width: 24px;
       height: 24px;
+      margin-left: 11px;
+    }
+    span {
+      color: #ffffff;
+      font-size: 18px;
+      flex: 1;
+      text-align: center;
     }
   }
-  .img_nav_box {
+  .box {
     width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    label {
-      width: 100px;
-      height: 100px;
-      img {
-        width: 100px;
-        height: 100px;
-      }
-      input {
-        display: none;
-        width: 100px;
-        height: 100px;
-      }
-    }
-  }
-  .btn_outer {
-    padding: 15px;
-    position: absolute;
-    bottom: 20px;
-    left: 0;
-    right: 0;
-    .btn {
-      box-sizing: border-box;
-
-      background: linear-gradient(
-        137deg,
-        rgba(233, 215, 177, 1) 0%,
-        rgba(200, 168, 113, 1) 100%
-      );
-      border-radius: 6px;
-      color: #705827;
-      padding: 10px 0;
+    padding: 0 15px;
+    box-sizing: border-box;
+    .nav_box {
       width: 100%;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      padding: 20px 0;
+      span {
+        display: inline-block;
+        width: 6px;
+        height: 15px;
+        background: linear-gradient(
+          180deg,
+          rgba(255, 147, 12, 1) 0%,
+          rgba(255, 80, 0, 1) 100%
+        );
+        border-radius: 5px;
+      }
+      p {
+        padding-left: 15px;
+        flex: 1;
+        color: #ffffff;
+        font-size: 16px;
+      }
+    }
+    .inp_box {
+      width: 100%;
+      padding: 10px 15px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background: rgba(40, 26, 0, 1);
+      opacity: 1;
+      border-radius: 6px;
+      input {
+        flex: 1;
+        padding: 0 10px 0 0;
+        border: none;
+        background: none;
+        color: #ffffff;
+        font-size: 16px;
+      }
+      input::-webkit-input-placeholder {
+        color: #ffffff;
+      }
+      img {
+        width: 24px;
+        height: 24px;
+      }
+    }
+    .img_nav_box {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      label {
+        width: 100px;
+        height: 100px;
+        img {
+          width: 100px;
+          height: 100px;
+        }
+        input {
+          display: none;
+          width: 100px;
+          height: 100px;
+        }
+      }
+    }
+    .btn_outer {
+      padding: 15px;
+      position: absolute;
+      bottom: 20px;
+      left: 0;
+      right: 0;
+      .btn {
+        box-sizing: border-box;
+
+        background: linear-gradient(
+          137deg,
+          rgba(233, 215, 177, 1) 0%,
+          rgba(200, 168, 113, 1) 100%
+        );
+        border-radius: 6px;
+        color: #705827;
+        padding: 10px 0;
+        width: 100%;
+      }
     }
   }
-}
+  .upload {
+    padding: 7px 15px;
+    display: flex;
+    flex-wrap: wrap;
+    label {
+        width: 80px;
+        height: 80px;
+        display: block;
+        background: url("../../../assets/img/tianjia.png") no-repeat center
+            center;
+        background-size: 100% 100%;
+    }
+    & > div {
+        margin: 0 15px 10px 0;
+        width: 80px;
+        height: 80px;
+        .pic {
+            width: 100%;
+            height: 100%;
+        }
+        position: relative;
+        .del {
+            width: 20px;
+            height: 20px;
+            position: absolute;
+            top: 2px;
+            right: 2px;
+        }
+    }
+  }
 </style>
